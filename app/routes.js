107d4d10905621d7573a083e69e484593e92bb91
jenrahmz <78032800+jenrahmz@users.noreply.github.com>
2021-06-07 16:33:43 +0100

    f-setup placeholder pages for v0.3

--- app/routes.js
+++ app/routes.js
@@ -70,3 +70,56 @@ router.post('/mys-version-0-2/choose-team-hyp', function (req, res) {
     res.redirect('#')
   }
 })
+
+
+// Branching v0.3
+router.post('/mys-version-0-3/find-out-about', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const findout = req.session.data['find-out']
+
+  if (findout=== 'find-out-hyp') {
+    res.redirect('/mys-version-0-3/which-team')
+  } 
+  else if (findout === 'find-out-metrics') {
+    res.redirect('/mys-version-0-3/service-metrics/which-service')
+  }
+  else if (findout === 'find-out-vision') {
+    res.redirect('/mys-version-0-3/vision/vision-heatmap')
+  }
+})
+
+
+router.post('/mys-version-0-3/choose-team-hyp', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const team = req.session.data['team']
+
+  if (team=== 'Royal Blue') {
+    res.redirect('/mys-version-0-3/performance-framework/royal-blue')
+  } 
+  else {
+    res.redirect('#')
+  }
+})
+
+
+
+router.post('/mys-version-0-3/service-metrics/choose-service', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const service = req.session.data['service']
+
+  if (service=== 'HAS') {
+    res.redirect('/mys-version-0-3/service-metrics/HAS-always-metrics')
+  } 
+  else {
+    res.redirect('#')
+  }
+})
